5929dcb71a5e222545cb68d156c6a58382458f03
xinlee2018 <929350040@qq.com>
2019-07-23 18:18:42 +0800

    lixin

---
+++ NENU_JoinMarket/src/main/java/com/nenu/market/service/preachinformation/PreachInformationService.java
@@ -0,0 +1,28 @@
+package com.nenu.market.service.preachinformation;
+
+import com.nenu.market.entity.PreachInformation;
+import com.nenu.market.entity.PreachInformation;
+
+import java.util.List;
+
+/**
+ * @Author: xinlee
+ * @Description:
+ * @Date: 20:49 2019/7/18
+ */
+public interface PreachInformationService {
+
+    public List<PreachInformation> listAll() throws Exception;
+
+    public boolean addUser(PreachInformation preachInformation) throws Exception;
+
+    public List<PreachInformation> searchByTime(String presentationTime) throws Exception;
+
+    public boolean updateAdvocate(String advocate, String presentationTime) throws Exception;
+
+    public boolean updateRecruitmentPosition(String recruitmentPosition, String presentationTime) throws Exception;
+
+    public boolean updateSigning(String signing, String presentationTime) throws Exception;
+
+    public boolean updatePlace(String place, String presentationTime) throws Exception;
+}
